97af601a3953ea5a0afeb2f8b45794edd6ef3698
TheDoctor205 <101677207+MateusSantos205@users.noreply.github.com>
2022-08-17 20:02:22 -0300

    tela de login finalizado

--- backend/_validar_login.php
+++ backend/_validar_login.php
@@ -3,11 +3,11 @@
 include '../backend/conexao.php';
 
 try{
-
+// //////////////////////////////
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
-
-    $sql = "SELECT *FROM tb_login WHERE email='$usuario' AND senha = '$senha'";
+// //////////////////////////////
+    $sql = "SELECT *FROM tb_login WHERE email='$usuario' AND senha = '$senha' AND ativo = 1";
 
     $comando = $con->prepare($sql);
 
@@ -15,7 +15,24 @@ try{
 
     $dados = $comando->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($dados);
+    // var_dump($dados);
+
+    // Verifica se existem registros dentro da variavel $dados
+
+    if($dados != null){  // "!=" diferente php
+    // se, usuario e senha foram validos, irá entrar nesse trecho de codigo
+
+    header('location: ../admin/gerenciar_viagens.php');
+
+    
+    }else{
+        // se, usuario e senha for invalidos, irá entrar nesse trecho de codigo
+
+        echo "Usuario ou senha invalidos";
+        // header('location: ../admin/gerenciar_viagens.php');
+    }
+
+   
     
 
 }catch(PDOException $erro){
